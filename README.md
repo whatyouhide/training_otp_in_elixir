39c7f8e914ee9dbe913e57d71e4cd411b2999cbb
Andrea Leopardi <an.leopardi@gmail.com>
2020-06-24 18:57:29 +0200

    Add tests and docker-compose stuff

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # OTP in Elixir
 
 Upcoming training.
+
+## Requirements
+
+  * Elixir 1.10
+  * Erlang/OTP 21 or later
+  * [Docker](https://www.docker.com/get-started)
+  * [Docker Compose](https://docs.docker.com/compose/install/)
---
+++ gen_server/redis_client_test.exs
@@ -0,0 +1,20 @@
+Code.require_file("redis_client_nonblocking.ex", __DIR__)
+
+ExUnit.start()
+
+defmodule RedisClientTest do
+  use ExUnit.Case
+
+  test "start connection and PING" do
+    assert {:ok, conn} = RedisClient.start_link(host: "localhost", port: 6379)
+
+    assert RedisClient.command(conn, ["PING"]) == "PONG"
+  end
+
+  test "GET + SET" do
+    assert {:ok, conn} = RedisClient.start_link(host: "localhost", port: 6379)
+
+    assert RedisClient.command(conn, ["SET", "mykey", "myvalue"]) == "OK"
+    assert RedisClient.command(conn, ["GET", "mykey"]) == "myvalue"
+  end
+end
